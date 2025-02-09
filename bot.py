10a8241c8963cbbb36a919b0bec167221eaa5678
nova <noreply@noreply>
2025-02-09 09:43:07 +0530

    fix

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from discord import app_commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-class Client(commands.Bot):  # Renamed to avoid confusion
+class Client(commands.Bot):  
     async def on_ready(self):
         print(f"Logged on as {self.user}")
         try:
@@ -18,20 +18,17 @@ class Client(commands.Bot):  # Renamed to avoid confusion
             print(f"error syncing command {e}")
 
 
-### dekh ye on_message wala jo hai naa function uska use karke bahoot kuch automate kar skte hai koi help likhega to ye wala trigger ho jayega wagera 
-   ## ye on_ready ya on_message ye naming hi use karna hai specific function hai kuch bhi nahi rakh skte  hai 
+
     async def on_message(self,msg):
         if msg.author==self.user:
             return
         if msg.content.startswith('hello'):
             await msg.channel.send(f"hi there {msg.author}")
-    # this is for adding message when user reacted to some message is ko haam like ( usr ko kuch denge option agar user ne rye react kiya to ye wala taks perform karo kuch aur react kiya to kuch aur karo wagera idea likh raha hu bas age dekh lena )
+    
     async def on_reaction_add(self,reaction,user):
         await reaction.message.channel.send(f"you reacted {user.name,reaction.emoji} ")
-# Use the custom class MyClient instead of the base discord.Client
 client=Client(command_prefix="!",intents=intents)
-# ye wala command banane ke liye 
-#953255670327693312
+
 GUILD_ID = discord.Object(id=953255670327693312)
 @client.tree.command(name="hello",description="say hello!",guild=GUILD_ID)
 async def sayHello(interaction:discord.Interaction):
